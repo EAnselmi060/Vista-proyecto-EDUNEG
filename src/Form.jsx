54ebad20169986c99b2b12eef8b975a46085a638
Ednoweri Anselmi <cedeeme95@gmail.com>
2024-12-08 16:02:25 -0400

    Update Form.jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -33,7 +33,7 @@ export const Form = () => {
     try {
       setLoading(true); 
 
-      const response = await fetch('https://backend-node-git-main-eanselmi060s-projects.vercel.app/profesores', {
+      const response = await fetch('https://backprueba-git-main-eanselmi060s-projects.vercel.app/profesores', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
